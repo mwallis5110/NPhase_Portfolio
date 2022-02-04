a66595567fa3f8922977fabf34c858772acea97f
Mason <mwallis5110@gmail.com>
2022-02-04 07:40:34 -0600

    Fixed loading animation styles

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,33 +1,27 @@
-import React, {useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 
 import LogoCircle from "../../assets/logo/logoCircle.png";
 
+import "./loading.css";
+
 export default function LoadingScreen() {
+  const imgRef = useRef();
 
-    const imgRef = useRef();
+  useEffect(() => {
+    gsap.to(imgRef.current, { duration: "2", opacity: "0" });
+  });
 
-    useEffect(() => {
-        gsap.to(imgRef.current, {duration: "2", opacity: "0"} )
-    })
-
-    return (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "20vh",
-          }}
-        >
-          <img
-            className="logoCircle"
-            ref={imgRef}
-            src={LogoCircle}
-            alt="Circle part of NPhase logo"
-            width="200"
-            height="200"
-          ></img>
-        </div>
-    );
+  return (
+    <div className="loadingScreen">
+      <img
+        className="logoCircle"
+        ref={imgRef}
+        src={LogoCircle}
+        alt="Circle part of NPhase logo"
+        width="200"
+        height="200"
+      ></img>
+    </div>
+  );
 }
