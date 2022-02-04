c264edc85d52b75451e6cd78086dc8c20fec4c05
Mason <mwallis5110@gmail.com>
2022-02-04 04:42:05 -0600

    First page done and cleaned up

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,42 +1,18 @@
-import React, {useRef, useEffect} from "react";
-import gsap from "gsap";
+import React from "react";
 
 import mainLogo from "../../assets/logo/mainLogo.png";
 
 import "./logo.css";
 
 export default function Logo() {
-
-    // const imgRef = useRef();
-
-    // useEffect(() => {
-    //   gsap.to(imgRef.current, { duration: "0", opacity: "0" });
-    // });
-
     return (
-      <div
-        style={{
-          position: "relative",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      // <div className="mainLogoDiv"
+      // >
         <img
           className="mainLogo"
-          //   ref={imgRef}
           src={mainLogo}
           alt="main logo"
-          width="400"
-          height="400"
-          style={{
-            marginTop: "25vh",
-            backgroundColor: "rgb(255,255,255,0.1)",
-            borderRadius: "10px",
-            // opacity: 0.9,
-            zIndex: 2
-          }}
         ></img>
-      </div>
+      // </div>
     );
 }
--- src/pages/firstPage/firstPage.js
+++ src/pages/firstPage/firstPage.js
@@ -1,6 +1,6 @@
-import React, {useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 
-import Logo from "../../components/logo/logo.js"
+import Logo from "../../components/logo/logo.js";
 
 import gsap from "gsap";
 
@@ -11,47 +11,36 @@ import ScrollButton from "../../components/scrollButton/scrollButton.js";
 import "./firstPage.css";
 
 export default function FirstPage() {
+  const firstPageRef = useRef();
 
-    const firstPageRef = useRef();
+  useEffect(() => {
+    gsap.fromTo(
+      firstPageRef.current,
+      { opacity: "0" },
+      { duration: "1", opacity: "1" }
+    );
+  });
 
-    useEffect(() => {
-        gsap.fromTo(firstPageRef.current, {opacity: "0"}, {duration: "1", opacity: "1"})
-    });
+  const startVideo = async () => {
+    const video = document.querySelector("#firstVideo");
 
-    const startVideo = async () => {
-      const video = document.querySelector("#firstVideo");
+    try {
+      await video.play();
 
-      try {
-        await video.play();
+      video.setAttribute("autoplay", true);
+    } catch (err) {
+      console.log(err, "video play error");
+      // do stuff in case your video is unavailable to play/autoplay
+    }
+  };
 
-        video.setAttribute("autoplay", true);
-
-      } catch (err) {
-        console.log(err, "video play error");
-        // do stuff in case your video is unavailable to play/autoplay
-      }
-    };
-
-    setTimeout(startVideo, 500);
+  setTimeout(startVideo, 500);
 
   return (
     <div className="firstPageWrapper">
       <div className="firstPageVid" ref={firstPageRef}>
-        <div id="navBar">
-          <Navbar />
-        </div>
-        <video
-          id="firstVideo"
-          // autoPlay
-          loop
-          muted
-          style={{
-            position: "relative",
-            width: "100%",
-            height: "100vh",
-            objectFit: "cover",
-          }}
-        >
+        <Navbar />
+        <video className="firstVideo" loop muted>
           <source src={MainVid} type="video/mp4" />
         </video>
         <div className="logoWrapper">
