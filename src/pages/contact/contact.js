2c77f5d385a69b6cb1537fccebccd1a2617f75ce
Mason Wallis <mwallis5110@gmail.com>
2022-10-02 13:29:19 -0500

    Contact form works

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 import SocialIcons from "../../components/socialIcons/socialIcons";
 import "./contact.css";
 
 export default function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_fjghc46",
+        "template_ozh1huy",
+        form.current,
+        "hOTuZfHLofJWCf3J9"
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contactWrapper" id="contactId">
       <h1 className="contactTitle">Get In Touch</h1>
@@ -11,24 +34,32 @@ export default function Contact() {
         <SocialIcons />
       </div>
       <div className="formWrapper">
-        <form className="contactForm">
-          <input id="name" type="text" placeholder="Name" required></input>
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+          <input
+            id="name"
+            type="text"
+            name="user_name"
+            placeholder="Name"
+            required
+          />
           <input
             id="email"
             type="text"
+            name="user_email"
             placeholder="Email Address"
             required
-          ></input>
+          />
           <textarea
             id="message"
             type="text"
+            name="message"
             rows="5"
             cols="70"
             placeholder="Message"
             required
-          ></textarea>
+          />
+          <input className="submitButton" type="submit" value="Send" />
         </form>
-        <button className="submitButton">Submit</button>
       </div>
     </div>
   );
